04de2c2973d0f2bdece67671fc44f8f9ea76b9f3
Roberts-sw <60960092+Roberts-sw@users.noreply.github.com>
2020-02-12 08:51:26 +0100

    Add files via upload

---
+++ mem_alloc.c
@@ -0,0 +1,138 @@
+/* lib > mem_alloc > mem_alloc.c
+   ------------------------------------------ TAB-size 4, code page UTF-8 --
+
+	Memory-allocator, idee:	https://web.ics.purdue.edu/~cs354/labs/lab6/
+
+Wijzigingen:
+	RvL 11 feb 2020	aanmaak
+   -------------------------------------------------------------------------
+*/
+#include"mem_alloc.h"
+
+	/* ---------------------------------------------------------
+	private
+	--------------------------------------------------------- */
+typedef struct st_mfb {struct st_mfb *next, *prev;} Smfb;
+typedef struct st_fb {size_t sz; Smfb;} Sfb;
+static size_t const OVH = offsetof(Sfb,d);		//bv: 4 (32-bit machine)
+static size_t const SMFB=sizeof(Smfb);			//bv: 8 (32-bit machine)
+#define _ao(n,o)		((size_t *)((char *)(n) + (o) &~(SMFB-1) ) )
+#define _fb(n,o)		((Sfb *)   ((char *)(n) + (o) &~(SMFB-1) ) )
+#define _nettosize(sz)	((sz) + SMFB-1 &~(SMFB-1) )	//data + ... ?
+static Sfb *freelist[32+1];	//index 0..31: blokken van SMFB*index Bytes
+							//index 32: groter, op grootte oplopend gesorteerd
+static void add_to_freelist (Sfb *n)
+{	int i=n->sz/SMFB;
+	_maximaal(i,_maxindex(freelist) );
+	Sfb *p=NULL, *q=freelist[i];
+	if(q)										//is er al een freelist[i]
+	{	if(_maxindex(freelist)==i)				//? en zijn het grote blokken
+			while(sz>q->sz)						//  ? zoek grootte-opvolger q
+				if(p=q, q=q->next, !q) break;	//	  ... en voorganger p
+		if(n->prev=p)		p->next=n;			//  Bestaat p ? n komt na p
+		else				freelist[i]=n;		//	: n is de eerste
+		if(n->next=q)		q->prev=n;			//	Bestaat q ? n komt voor q
+	} else freelist[i]=n, n->prev=n->next=NULL;
+}
+static Sfb *find_in_freelist (size_t sz)
+{	sz=_nettosize(sz);
+	Sfb *p, *q=freelist[_maxindex(freelist)];
+	for(p=freelist[sz/SMFB]; p<q; p++)	if(p)	return p;//vaste grootte
+	if(q)	while(q=q->next)	if(sz<=q->sz)	return q;//gesorteerde grootte
+	return NULL;
+}
+void get_from_freelist (Sfb *n)					//haal blok tussen p en q uit
+{	Sfb *p=n->prev, *q=n->next;
+	if(p)	p->next=q;
+	if(q)	q->prev=p;
+	n->_next=n->prev=NULL;
+}
+
+static void merge (Sfb *n, Sfb *q, int allocate)
+{	size_t nsz=(!q?0:OVH*2+q->sz&~1) + n->sz&~1;//nsz => netto size
+	n->sz=*_ao(n,nsz+OVH)=nsz | !!allocate;		//beschrijf hekken
+}
+static Sfb *split (Sfb *n, size_t nss)			//nss: netto splitted size
+{	if(nss+OVH*2<=(n->sz&~1) )
+	{	size_t sz=(n->sz&~1)-(nss+OVH*2);
+		n->sz=*_ao(n, sz+OVH)= sz;
+		n=_fb(n,sz+OVH*2);						//voorkant ingekort, n erachter
+		n->sz=*_ao(n,nss+OVH)=nss;
+		return n;								//achterkant gemaakt
+	}	return NULL;
+}
+static size_t const MEM = _nettosize(1L<<12);	//4kB
+static size_t mem[MEM/sizeof(size_t)];
+
+	/* ---------------------------------------------------------
+	public
+	--------------------------------------------------------- */
+void mem_init (void)
+{	mem[0]=mem[_maxindex(mem)-0]=1;				//2 loze randhekken
+	mem[1]=mem[_maxindex(mem)-1]=MEM-OVH*4;		//2 blokhekken om netto-blok
+	add_to_freelist((Sfb *)(mem+1) );
+}/*	met 4B-pointers => 0:1 4:4080 8:NULL 12:NULL ... 4088:4080 4092:1	*/
+void *mem_malloc (size_t size)
+{	if(size)
+	{	Sfb *n, *q;
+		if( n=find_in_freelist(size) )			//a.
+		{	get_from_freelist(n);				//b.
+			if(n->sz>OVH*2+_nettosize(size) )	//c.
+				if(q=split(n,n->sz-OVH*2) )
+					add_to_freelist(q);
+			merge(n,NULL,1);					//d.
+			return _ao(n,OVH);
+		}
+	}	return NULL;
+}/*	bv:	mem_malloc(100) na mem_init als bovenstaand:
+	4:105 ... 112:105 116:3968 120:NULL 124:NULL ... 4088:3968	*/
+void *mem_calloc (size_t nitems, size_t size)
+{	void *p=mem_malloc(nitems*size);
+	if(p)	memset(p,0,nitems*size);
+	return p;
+}
+void mem_free (void *addr)
+{	if(!(_ao(addr,-OVH)&1) )	return;
+	merge(n,NULL,0);							//a.
+	Sfb *p, *q, *n=_fb(addr,-OVH);	size_t sz;
+	if(p=_fb(n,-OVH), sz=p->sz, 	!(sz&1) )	//	vrije voorbuur
+	{	get_from_freelist(p); merge(p,n,0);n=p;	}
+	if(sz=n->sz, q=_fb(n,sz+OVH*2), !(q->sz&1) )//	vrije achterbuur
+	{	get_from_freelist(q); merge(n,q,0);		}
+	add_to_freelist(n);							//b.
+}
+void *mem_realloc (void *addr, size_t size)
+{	if(!addr)	return mem_malloc(size);			//a.
+	if(!size)	return mem_free(addr), NULL;	//b.
+	Sfb *n=_fb(addr,-OVH);	size_t nsz=n->sz & ~1;
+	Sfb *q=_fb(n,nsz+OVH*2);size_t netto=_nettosize(size);
+	if(nsz>=OVH*2+netto)						//c.
+	{	if(!(q->sz&1) )							//	vrije opvolger
+		{	p=split(n,nsz-OVH*2-netto);
+			get_from_freelist(q); merge(p,q,0);
+			add_to_freelist(p);
+		} else if(nsz>OVH*2+netto)				//	vrij restblok
+		{	p=split(n,nsz-OVH*2-netto);
+			add_to_freelist(p);
+		}	return addr;
+	}											//d. groter blok nodig:
+	size_t psz=_fb(n,-OVH)->sz;					if(psz&1) psz=0;//(niet-vrij)
+	if(!(q->sz&1) )								//	vrije opvolger
+	{	if(netto<=nsz+q->sz+OVH*2)
+		{	get_from_freelist(q);	merge(n,q,1);
+			return addr;
+		} else if(netto<=psz+OVH*2+nsz+q->sz+OVH*2)//icm. vrije voorganger:
+		{	get_from_freelist(q);	merge(n,q,0);//		eerst achterkant doen
+			goto ap;
+		}
+	} else if(netto<=psz+OVH*2+nsz)				//	vrije voorganger
+ap:	{	Sfb *p=_fb(n,-psz-OVH*2);
+		get_from_freelist(p);	merge(p,n,1);
+		memcpy(_ao(p,OVH), addr, nsz);
+		return _ao(p,OVH);
+	}
+	if(void *vp=mem_malloc(size) )				//e.
+	{	memcpy(vp, addr, nsz);
+		return mem_free(addr), vp;
+	}	return NULL;							//f.
+}
---
+++ mem_alloc.h
@@ -0,0 +1,72 @@
+/* lib > mem_alloc > mem_alloc.h
+   ------------------------------------------ TAB-size 4, code page UTF-8 --
+Achtergrond:
+	Een programma in RAM heeft geheugenindeling: text data bss heap ... stack
+		text = programmacode
+		data = geïnitialiseerd geheugen
+		bss = block started by symbol: 0-geïnitialiseerd voordat main() begint.
+		heap ... stack = dynamische geheugenruimte
+	
+	Oude programmatuur gebruikte brk() ter definitie van een 0-geïnitialiseerd
+	geheugenblok en sbrk() om de grootte aan te passen. Bij een microcontroller,
+	zonder virtueel geheugen, is beheer van een vast bss-blok zinvoller.
+
+Werking:
+	Een beheerd geheugenblok heeft aan voor- en achterzijde een beschermingshek.
+	Een hek bevat de (even) beheerde grootte en als lsb een "toegewezen"-bit.
+	Een vrij (niet-toegewezen) blok maakt deel uit van een DLL-"freelist" en
+	bevat 2 pointers *next en *prev.
+	Daardoor is de minimaal toewijsbare grootte gelijk aan 2*sizeof(void *).
+	Met de eis dat elk blok zowel voorganger als opvolger heeft, zijn er aan de
+	geheugenuiteinden nog loze hekken met waarde 1: 0 Bytes, niet-beschikbaar.
+
+Interface-beschrijving:
+	void mem_init(void)
+	a. Plaats loze hekken aan de uiteinden van het beheerde geheugen.
+	b. Maak één geheugenblok dat hier exact tussen past, zet het in freelist[].
+
+	void *mem_malloc(size_t sz)
+	a. Zoek een blok p in freelist[] waarin sz past. Mislukking levert NULL.
+	b. Haal het blok p uit freelist[].
+	c. Bij genoeg overruimte voor een restblok, splits een vrij blok n af.
+	d. Neem p in gebruik en geef het eerste bruikbare adres als resultaat.
+
+    void *mem_calloc(size_t nitems, size_t itemsize)
+    a. Indien mem_malloc(nitems*itemsize) lukt, vult het resultaatblok met 0.
+
+	void mem_free(void *addr)
+	a. Maak het blok vrij, en voeg samen met eventuele vrije voor-/achterbuur.
+	b. Plaats het (aangepaste) blok in freelist[].
+
+	void *mem_realloc(void *addr, size_t sz)
+	a. NULL==addr gedraagt zicht als mem_malloc(sz) 
+	b. 0==sz roept mem_free(addr) aan en levert NULL;
+	c. Bij kleinere sz dan huidige netto-grootte wordt geprobeerd om het restant
+		af te splitsen en eventueel met een vrije achterbuur samen te voegen.
+		Anders wordt het blok gewoon teruggegeven.
+	d. Bij grotere sz wordt gepoogd om het blok met een vrije achterbuur samen
+		te voegen, eventueel met een vrije voorbuur erbij en verplaatsen van
+		de inhoud. Het eerste bruikbare adres wordt teruggegeven.
+	e. Anders wordt gekeken of mem_malloc() lukt en de inhoud
+		verplaatst naar het nieuwe blok. Het nieuwe adres wordt teruggegeven.
+	f. Anders wordt NULL teruggegeven.
+
+Wijzigingen:
+	RvL 11 feb 2020	aanmaak
+   -------------------------------------------------------------------------
+*/
+#include <stddef.h>			//offsetof, size_t
+
+	//algemene macro's:
+#define _elementen(a)		( sizeof(a)/sizeof(*(a)) )
+#define _maxindex(a)		(_elementen(a)-1)
+#define _maximaal(x,M)		if(x>(M) ) x=(M)
+
+	/* ---------------------------------------------------------
+	interface
+	--------------------------------------------------------- */
+void mem_init(void);
+void *mem_malloc(size_t totalsize);
+void *mem_calloc(size_t nitems, size_t itemsize);
+void mem_free(void *addr);
+void *mem_realloc(void *addr, size_t size);
